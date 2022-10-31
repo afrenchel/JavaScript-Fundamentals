972521a8834bffcb55679ec24472e54b2f7b83cb
Alina Frenchel <alina.frenchel@gmail.com>
2022-10-31 13:36:22 +0200

    Creating a modal-Working with classes

---
+++ 06-Modal/starter/script.js
@@ -0,0 +1,21 @@
+'use strict';
+//we select the elements and store them above in variables to have them
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal'); //multiple elements, only the first one will get selected querySelectorAll
+const btnShowModal = document.querySelectorAll('.show-modal');
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
