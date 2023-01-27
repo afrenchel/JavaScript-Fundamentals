b9024c5b9611bd61c8cadb54c6e4c09945413bf7
Alina Frenchel <alina.frenchel@gmail.com>
2023-01-27 16:09:17 +0200

    Add recipe functionality-Finishing the Forkify project

--- Forkify Project/src/js/config.js
+++ Forkify Project/src/js/config.js
@@ -2,3 +2,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'; //using uppercase
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '88b5e071-a248-46e0-9b65-dc668d687738';
+export const MODAL_CLOSE_SEC = 2.5;
