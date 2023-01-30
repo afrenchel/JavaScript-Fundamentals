b62dc6fc9beef90b1e5d5311bd7860de305325c7
Alina Frenchel <alina.frenchel@gmail.com>
2023-01-30 15:54:37 +0200

    Changing Fractional with Fracty

--- Forkify Project/src/js/views/recipeViews.js
+++ Forkify Project/src/js/views/recipeViews.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 
-import icons from '../../img/icons.svg'; //Parcel 2
-import { Fraction } from 'fractional';
+import icons from '../../img/icons.svg'; //Parcel 2npm instal
+import fracty, { Fracty } from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -145,7 +145,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? fracty(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
